b00b4c0c473f1619df4a1b588229f25bb8b40428
jagadeshbalasubramaniam <jagadesh.trupur@gmail.com>
2018-03-07 22:36:56 +0530

    First commit

--- src/main/java/com/stepdefinition/TestRunner.java
+++ src/main/java/com/stepdefinition/TestRunner.java
@@ -12,25 +12,25 @@ import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "src\\test\\resources\\feature\\FlightStatus.feature",glue = { "com.stepdefinition" }, plugin = {
+@CucumberOptions(features = "src\\test\\resources\\feature\\Flipkart.feature",glue = { "com.stepdefinition" }, plugin = {
 		"com.resources.ExtentCucumberFormatter:"} )
 
 public class TestRunner {
 
-	@BeforeClass
-	public static void beforeClass() {
-		LocalDateTime now = LocalDateTime.now();
-		int year = now.getYear();
-		int month = now.getMonthOfYear();
-		int day = now.getDayOfMonth();
-		int hour = now.getHourOfDay();
-		int minute = now.getMinuteOfHour();
-		int second = now.getSecondOfMinute();
-		String snewFilename1 = year + "_" + month + "_" + day + "_" + hour + "_" + minute + "_" + second;
-		String filePath = System.getProperty("user.dir") + "/src/main/resources/AutomationSuite/Report_"
-				+ snewFilename1 + ".html";
-		File file = new File(filePath);
-		ExtentCucumberFormatter.setExtentHtmlReport(file);
-	}
+	//*@BeforeClass
+	//public static void beforeClass() {
+		//LocalDateTime now = LocalDateTime.now();
+		//int year = now.getYear();
+		//int month = now.getMonthOfYear();
+		//int day = now.getDayOfMonth();
+		//int hour = now.getHourOfDay();
+		//int minute = now.getMinuteOfHour();
+		//int second = now.getSecondOfMinute();
+		//String snewFilename1 = year + "_" + month + "_" + day + "_" + hour + "_" + minute + "_" + second;
+		//String filePath = System.getProperty("user.dir") + "/src/main/resources/AutomationSuite/Report_"
+		//		+ snewFilename1 + ".html";
+		//File file = new File(filePath);
+		//ExtentCucumberFormatter.setExtentHtmlReport(file);
+	//}
 	
 }
